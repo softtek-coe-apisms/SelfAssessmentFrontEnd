950c311fc7aa8c24ef2ce78c58ab7c4e412a4716
Francisco Alejo Villanueva Vázquez <francisco.villanueva@softtek.com>
2020-05-07 16:49:59 -0500

    Front

---
+++ SelfAssessmentFront/Controllers/InterviewerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using InterviewFront.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAssessmentFront.Controllers
+{
+    public class InterviewerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Interview()
+        {
+            return View();
+        }
+
+        public IActionResult SearchCandidate()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        public IActionResult SelfAssessment()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
