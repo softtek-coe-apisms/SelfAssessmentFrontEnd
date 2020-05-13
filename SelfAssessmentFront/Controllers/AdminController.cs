9354f096660395ac2d1003d7ebaa968a6173e3b1
Alejandro <asber_dhc@hotmail.es>
2020-05-13 14:35:06 -0500

    refactor layout

--- SelfAssessmentFront/Controllers/AdminController.cs
+++ SelfAssessmentFront/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SelfAssessmentFront.ViewModels;
 
 namespace SelfAssessmentFront.Controllers
 {
@@ -13,9 +14,18 @@ namespace SelfAssessmentFront.Controllers
             return View();
         }
 
-        public IActionResult Users()
+        public IActionResult Customers()
         {
             return View();
+        }
+
+        public List<CustomersViewModel> Customerss()
+        {
+            List<CustomersViewModel> customers = new List<CustomersViewModel> {
+                new CustomersViewModel { Id = 1, Name = "Ale" },
+                new CustomersViewModel { Id = 2, Name = "Juan" },
+            };
+            return customers;
         }
     }
 }
---
+++ SelfAssessmentFront/ViewModels/CustomersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelfAssessmentFront.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
