519aa9b894101c72dc6d2484b884b0e6fe8edf23
JAIME LOPEZ VELOZ <jaime.lopezv@softtek.com>
2020-07-02 14:28:30 -0500

    Front vistas jaime

---
+++ SelfAssessmentFront/Controllers/SkillCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAssessmentFront.Controllers
+{
+    public class SkillCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SkillCategoryList()
+        {
+            return View();
+        }
+    }
+}
